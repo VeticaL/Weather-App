cd08be9bc6259f75404969c0b15b65b550eb54b8
Vertical <ambintasso@gmail.com>
2023-01-14 13:24:59 +0530

    toggle switch

--- script.js
+++ script.js
@@ -2,27 +2,93 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const errormsg = document.querySelector(".error");
 const condition = document.querySelector(".condition");
-const Location = document.querySelector(".location");
+const city = document.querySelector(".location");
 const temperature = document.querySelector(".temperature");
 const feelsLike = document.querySelector(".feels-like");
 const humidity = document.querySelector(".humidity");
 const chanceofRain = document.querySelector(".chance-of-rain");
 const windSpeed = document.querySelector(".wind-speed");
+const toggle = document.querySelector(".toggle");
+const search = document.querySelector(".submit");
 
-async function weather() {
+let unit = "imperial";
+let loc = "London";
+
+async function weather(location) {
   let response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=94651adb8f2bf0b308284bc74dc6719a`
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      location +
+      "&units=" +
+      unit +
+      "&APPID=94651adb8f2bf0b308284bc74dc6719a",
+    { mode: "cors" }
   );
-  let weatherData = await response.json();
-  console.log(weatherData);
+  let data = await response.json();
+  let name = data.name;
+  let weather = data.weather[0].description;
+  let temp = data.main.temp;
+  let feels_like = data.main.feels_like;
+  let humidity_data = data.main.humidity;
+  let chanceofRain_data = data.rain;
+  let windSpeed_data = data.wind.speed;
+  updateData(
+    name,
+    weather,
+    temp,
+    feels_like,
+    humidity_data,
+    chanceofRain_data,
+    windSpeed_data
+  );
+  console.log(data);
 }
 
-function getLocation() {
-  if (input.value !== "") {
-    let search = input.value;
-  } else if (input.value === "") {
-    errormsg.textContent = "Enter a valid location";
+function updateData(name, weather, temp, fells, humid, rain, wind) {
+  city.textContent = name;
+  condition.textContent = weather.toUpperCase();
+  if (unit === "imperial") {
+    temperature.textContent = temp + " °F";
+    feelsLike.textContent = "Feels Like: " + fells + " °F";
+    humidity.textContent = "Humidity: " + humid + " %";
+    chanceofRain.textContent = "Chance of Rain: " + rain;
+    windSpeed.textContent = "Wind Speed: " + wind + " mph";
+  } else if (unit === "metric") {
+    temperature.textContent = temp + " °C";
+    feelsLike.textContent = "Feels Like: " + fells + " °C";
+    humidity.textContent = "Humidity: " + humid + " %";
+    chanceofRain.textContent = "Chance of Rain: " + rain;
+    windSpeed.textContent = "Wind Speed: " + wind + " kmph";
   }
 }
 
-weather();
+weather(loc);
+
+toggle.addEventListener("click", () => {
+  if (unit === "imperial") {
+    unit = "metric";
+    weather(loc);
+  } else if (unit === "metric") {
+    unit = "imperial";
+    weather(loc);
+  }
+});
+
+search.addEventListener("click", (e) => {
+  e.preventDefault();
+  loc = input.value;
+  weather(loc);
+});
+
+input.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    search.click();
+  }
+});
+
+
+// TO-Do
+// styling
+// chance of Rain
+// unit positoining
+// local storage
